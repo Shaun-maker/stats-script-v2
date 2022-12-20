e63125de541c4435cc04719a986c96354cb15a90
Axel Paillaud <a.paillaud75@gmail.com>
2022-12-20 09:54:18 +0100

    change some global variable to local variable

--- content_scripts/cpy_stats.js
+++ content_scripts/cpy_stats.js
@@ -1,12 +1,12 @@
-var allTable = document.getElementsByTagName('table');
+let allTable = document.getElementsByTagName('table');
 
-var statsTable = allTable[1];
-var innerText = statsTable.innerText;
+let statsTable = allTable[1];
+let innerText = statsTable.innerText;
 
-var lines = innerText.split('\n');
+let lines = innerText.split('\n');
 lines.splice(0,4);
 
-var newText = lines.join('\n');
+let newText = lines.join('\n');
 newText = newText.trimEnd();
 
 var sendStats = browser.runtime.sendMessage(newText);
